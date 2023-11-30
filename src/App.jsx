d8a6c26b00ae129e49fef12227796c11b689cab0
Sanjaee <afrizaahmad18@gmail.com>
2023-11-30 18:36:24 +0700

    r

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,103 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-const Downloader = () => {
-  const [url, setUrl] = useState("");
-  const [downloadInfo, setDownloadInfo] = useState(null);
-  const [loading, setLoading] = useState(false);
+const YoutubeVideo = () => {
+  const [videoId, setVideoId] = useState("");
+  const [videoData, setVideoData] = useState(null);
+  const [error, setError] = useState(null);
 
-  const handleUrlChange = (e) => {
-    setUrl(e.target.value);
+  const extractVideoId = (url) => {
+    const regex =
+      /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/;
+    const match = url.match(regex);
+
+    return match ? match[1] : null;
   };
 
-  const handleDownload = async () => {
-    setLoading(true);
+  const handleInputChange = (e) => {
+    const inputUrl = e.target.value;
+    const extractedVideoId = extractVideoId(inputUrl);
 
+    if (extractedVideoId) {
+      setVideoId(extractedVideoId);
+    } else {
+      setVideoId("");
+    }
+  };
+
+  const handleFetchData = async () => {
     const options = {
       method: "GET",
-      url: "https://youtube-mp3-downloader2.p.rapidapi.com/ytmp3/ytmp3/",
-      params: {
-        url: url,
-      },
+      url: "https://youtube-mp36.p.rapidapi.com/dl",
+      params: { id: videoId },
       headers: {
         "X-RapidAPI-Key": "7cb05360f8mshca6918f8ea33103p1c4264jsn97b178de78f8",
-        "X-RapidAPI-Host": "youtube-mp3-downloader2.p.rapidapi.com",
+        "X-RapidAPI-Host": "youtube-mp36.p.rapidapi.com",
       },
-      responseType: "json",
     };
 
     try {
       const response = await axios.request(options);
-
-      // Assuming the API response structure contains the necessary information
-      const result = {
-        title: response.data.title,
-        link: response.data.link,
-        length: response.data.length,
-        size: response.data.size,
-      };
-
-      setDownloadInfo(result);
+      setVideoData(response.data);
+      setError(null);
     } catch (error) {
       console.error(error);
-    } finally {
-      setLoading(false);
+      setError("Error fetching video data");
+      setVideoData(null);
     }
   };
 
-  const handleRedirect = () => {
-    if (downloadInfo) {
-      window.location.href = downloadInfo.link;
+  const handleDownload = () => {
+    if (videoData && videoData.link) {
+      const link = document.createElement("a");
+      link.href = videoData.link;
+      link.download = "downloadedFile.mp3";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
     }
   };
 
   return (
-    <div className="container">
-      <h1>YouTube Video Downloader</h1>
-      <form>
-        <label>
-          YouTube URL limit 20 url:
-          <input type="text" value={url} onChange={handleUrlChange} />
-        </label>
-        <button onClick={handleDownload} disabled={loading}>
-          {loading ? "Downloading..." : "Download"}
-        </button>
-      </form>
+    <body>
+      <div className="login-container">
+        <form className="login-form">
+          <h1>YouTube Video Downloader</h1>
+          <label>
+            Video URL:
+            <input
+              type="text"
+              value={videoId}
+              onChange={handleInputChange}
+              className="input-field"
+              placeholder="Enter YouTube Video URL"
+            />
+          </label>
+          <button onClick={handleFetchData} className="submit-button">
+            Generate
+          </button>
 
-      {downloadInfo && (
-        <div className="result">
-          <h2>Download Information</h2>
-          <div className="result-container">
-            <p>Title: {downloadInfo.title}</p>
-            <p>Length: {downloadInfo.length}</p>
-            <p>Size: {downloadInfo.size}</p>
-            <button onClick={handleRedirect}>Download Now</button>
-          </div>
-        </div>
-      )}
-    </div>
+          {error && <p className="error-message">{error}</p>}
+
+          {videoData && (
+            <div className="result-container">
+              <h2>Result</h2>
+              <p>Link: {videoData.link}</p>
+              <p>Title: {videoData.title}</p>
+              <p>Filesize: {videoData.filesize}</p>
+              <p>Progress: {videoData.progress}</p>
+              <p>Duration: {videoData.duration}</p>
+              <p>Status: {videoData.status}</p>
+              <p>Message: {videoData.msg}</p>
+              <button onClick={handleDownload} className="download-button">
+                Download
+              </button>
+            </div>
+          )}
+        </form>
+      </div>
+    </body>
   );
 };
 
-export default Downloader;
+export default YoutubeVideo;
